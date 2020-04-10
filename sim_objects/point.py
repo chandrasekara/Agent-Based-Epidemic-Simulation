f83df7d6f402d8659de4a30e1dc0651332182d93
Dhilan Chandrasekara <dhilan.chandrasekara2@gmail.com>
2020-04-10 18:56:52 +1000

    add update method for point

--- sim_objects/point.py
+++ sim_objects/point.py
@@ -1,7 +1,25 @@
 from random import random
 from simulation_object import SimulationObject
+import math
+
+MAX_RADIANS = 2 * math.pi
+
+MAX_VELOCITY = 10
 
 class Point(SimulationObject):
+
+    def __init__(self, imageFile, x_in, y_in, specified_velocity=None):
+        SimulationObject.__init__(imageFile, x_in, y_in)
+        if specified_velocity == None:
+            self.velocity = (random()*MAX_DEGREES, MAX_VELOCITY/2)
+
+    def update():
+        SimulationObject.__init__()
+        direction = velocity[0]
+        speed = velocity[1]
+        self.x += math.cos(direction) * speed
+        self.y += math.sin(direction) * speed
+
 
     def thing(self):
         pass
