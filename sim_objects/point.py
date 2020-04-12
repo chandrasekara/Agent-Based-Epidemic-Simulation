8639efe976c0f61c638bfbf4e9683f5e541902f6
Dhilan Chandrasekara <dhilan.chandrasekara2@gmail.com>
2020-04-12 17:23:34 +1000

    code cleanup

--- sim_objects/point.py
+++ sim_objects/point.py
@@ -1,27 +1,33 @@
 from random import random
 from simulation_object import SimulationObject
 import math
+from util.game_info import *
 
-MAX_RADIANS = 2 * math.pi
-
-MAX_VELOCITY = 10
+MAX_VELOCITY = 2
 
 class Point(SimulationObject):
 
     def __init__(self, imageFile, x_in, y_in, specified_velocity=None):
         SimulationObject.__init__(self, imageFile, x_in, y_in)
         if specified_velocity == None:
-            self.velocity = (random()*MAX_RADIANS, MAX_VELOCITY/2)
+            speed_x = MAX_VELOCITY * random()
+            self.velocity = (speed_x, math.sqrt(MAX_VELOCITY*MAX_VELOCITY - speed_x*speed_x))
+        else:
+            self.velocity = specified_velocity
 
     def update(self):
         SimulationObject.update(self)
-        direction = self.velocity[0]
-        speed = self.velocity[1]
-        self.x += math.cos(direction) * speed
-        self.y += math.sin(direction) * speed
+        if self.x >= DISPLAY_WIDTH or self.x <= 0:
+            self.bounce('x')
+        if self.y >= DISPLAY_HEIGHT or self.y <= 0:
+            self.bounce('y')
+        dx = self.velocity[0]
+        dy = self.velocity[1]
+        self.y += dy
+        self.x += dx
 
-
-    def thing(self):
-        pass
-
-
+    def bounce(self, direction):
+        if direction == 'x':
+            self.velocity = (-self.velocity[0], self.velocity[1])
+        else:
+            self.velocity = (self.velocity[0], -self.velocity[1])
