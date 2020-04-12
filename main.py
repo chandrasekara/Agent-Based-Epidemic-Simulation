610c2af03b5fcfc74c358d2780e95c115b773a9f
Dhilan Chandrasekara <dhilan.chandrasekara2@gmail.com>
2020-04-12 17:40:52 +1000

    remove hardcoded variables and replace with references to util files

--- main.py
+++ main.py
@@ -3,35 +3,21 @@ from pygame.locals import *
 from random import random
 import time
 from sim_objects.point import Point
+from sim_objects.util.game_info import *
+from sim_objects.util.colors import *
 
 pygame.init()
 
-display_width = 640
-display_height = 480
-
-# colour defs, should be in it's own class/file
-
-black = (0,0,0)
-white = (255,255,255)
-
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('COVID-19 Simulation')
+gameDisplay = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pygame.display.set_caption('Epidemic Simulation')
 clock = pygame.time.Clock()
-
-circleImg = pygame.image.load('static/ball.png')
-
 
 point_objs = []
 
 for i in range(10):
-    point_objs.append(Point('static/ball.png', random()*display_width,random()*display_height))
+    point_objs.append(Point(BALL_SPRITE, random()*DISPLAY_WIDTH,random()*DISPLAY_HEIGHT))
 
 running = 1
-
-# really should be using obj oriented for this sort of stuff lol
-
-circ_x = 300
-circ_y = 300
 
 while running:
   for event in pygame.event.get():
@@ -47,6 +33,5 @@ while running:
       point.display(gameDisplay)
   pygame.display.update()
   clock.tick(60)
-
 
 pygame.quit()
--- sim_objects/util/game_info.py
+++ sim_objects/util/game_info.py
@@ -1,3 +1,7 @@
 DISPLAY_WIDTH = 640
 
 DISPLAY_HEIGHT = 480
+
+BALL_SPRITE = "static/ball.png"
+
+BALL_INFECTED_SPRITE = "static/ball-infected.png"
