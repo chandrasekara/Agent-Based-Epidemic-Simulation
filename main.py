70f7305a46d30e29ae18d7199b74d8a36a285e33
Dhilan Chandrasekara <dhilan.chandrasekara2@gmail.com>
2020-04-18 22:24:55 +1000

    add social distancing factor

--- main.py
+++ main.py
@@ -10,21 +10,26 @@ import argparse
 DEFAULT_NUM_AGENTS = 80
 DEFAULT_INITIAL_INFECTED = 3
 DEFAULT_INFECTION_RECOVERY_PERIOD = 200
+DEFAULT_SOCIAL_DISTANCING_FACTOR = 50
 
 parser = argparse.ArgumentParser(description='Simulate the spread of an infectious virus')
 parser.add_argument('-a', '--agents', type=int, help='Number of Agents')
 parser.add_argument('-i', '--infected', type=int, help='Number of Initially Infected Agents')
 parser.add_argument('-r', '--recovery', type=int, help='The number of ticks for an infected agent to recover')
+parser.add_argument('-d', '--social-distancing', type=int, help='The percentage of agents that follow social' +
+                                                               'distancing', dest="social_distancing_factor")
 parser.add_argument('--default', help='use the default settings', action='store_true')
 args = parser.parse_args()
 
 
 # TODO: Move these functions to a better place
-def initialize_simulation_objects(number_of_agents, initial_infected, infection_recovery_period):
+def initialize_simulation_objects(number_of_agents, initial_infected, infection_recovery_period,
+                                  social_distancing_factor):
     sim_objs = []
     for i in range(number_of_agents):
         sim_objs.append(
-            Point(BALL_SPRITE, random() * DISPLAY_WIDTH, random() * DISPLAY_HEIGHT, infection_recovery_period))
+            Point(BALL_SPRITE, random() * DISPLAY_WIDTH, random() * DISPLAY_HEIGHT, infection_recovery_period,
+                  social_distancing_factor))
     for i in range(initial_infected):
         sim_objs[i].infect()
     return sim_objs
@@ -34,8 +39,10 @@ if args.default:
     number_of_agents = DEFAULT_NUM_AGENTS
     initial_infected = DEFAULT_INITIAL_INFECTED
     infection_recovery_period = DEFAULT_INFECTION_RECOVERY_PERIOD
+    social_distancing_factor = DEFAULT_SOCIAL_DISTANCING_FACTOR
     # TODO: Move to better global game information object
-    sim_objs = initialize_simulation_objects(number_of_agents, initial_infected, infection_recovery_period)
+    sim_objs = initialize_simulation_objects(number_of_agents, initial_infected, infection_recovery_period,
+                                             social_distancing_factor)
 else:
     number_of_agents = args.agents
     initial_infected = args.infected
