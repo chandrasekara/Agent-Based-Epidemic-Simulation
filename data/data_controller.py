afcd49591ca18bc46d2eb8cbead7e6a6316470f4
Dhilan Chandrasekara <dhilan.chandrasekara2@gmail.com>
2020-04-23 19:49:35 +1000

    chore: add comment to data controller method

--- data/data_controller.py
+++ data/data_controller.py
@@ -5,6 +5,7 @@ class DataController:
         self.f.write('Time,Infected,Recovered,Never Infected\n')
 
     def tick(self, infected_number, recovered_number, never_infected_number):
+        # record simulation result for one unit of time
         self.clock += 1
         self.f.write(str(self.clock) + "," + str(infected_number) + "," + str(recovered_number) + "," + str(never_infected_number) + "\n")
 
